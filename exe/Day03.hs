cfb0301657362b6268a4d3d283fb871b0c71d068
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-12-14 11:56:14 +0000

    Solve Day03b

--- exe/Day03.hs
+++ exe/Day03.hs
@@ -1,29 +1,35 @@
-module Main where
+module Day03 where
 
 -- Base
+import Control.Applicative ((<|>))
 import Data.Array (Array, bounds, (!))
 import Data.Char (isDigit)
+import Data.Maybe (fromMaybe, catMaybes)
 
 -- Mine
-import Common (adventOfCode, Input, arrayParse)
+import Common (adventOfCode, arrayParse)
 
 type Map = Array (Int, Int) Char
 
-day03a :: Map -> String
-day03a = show . sum . findNumbers (1, 1) "" False
-
-day03b :: Map -> String
-day03b = show . sum . findGears (1, 1)
-
 isSymbol :: Char -> Bool
 isSymbol x = x /= '.' && not (isDigit x)
+
+inBounds :: (Int, Int) -> (Int, Int) -> Bool
+inBounds (h, w) (r, c) = r <= h && r >= 1 && c <= w && c >= 1
+
+inBoundsAndDigit :: (Int, Int) -> Map -> Bool
+inBoundsAndDigit (r, c) m = inBounds (h, w) (r, c) && isDigit (m ! (r, c))
+  where (_, (h, w)) = bounds m
+
+day03a :: Map -> String
+day03a = show . sum . findNumbers (1, 1) "" False
 
 findNumbers :: (Int, Int) -> String -> Bool -> Map -> [Int]
 findNumbers (r, c) digString doesCount m
   | r == h && c == w + 1           = []
   | c == w + 1 || not (isDigit ch) = accumNumber digString doesCount (findNumbers next  "" False m)
   | otherwise                      = findNumbers next (ch:digString) (doesCount || check (r, c) m) m
-  where 
+  where
     (_, (h, w)) = bounds m
     ch = m ! (r, c)
 
@@ -35,34 +41,62 @@ findNumbers (r, c) digString doesCount m
     next = if c == w + 1 then (r + 1, 1) else (r, c + 1)
 
 check :: (Int, Int) -> Map -> Bool
-check (r, c) m = any (checkOne) [(r + dr, c + dc)| (dr, dc) <- ds]
+check (r, c) m = any checkOne [(r + dr, c + dc)| (dr, dc) <- ds]
   where
     ds :: [(Int, Int)]
-    ds = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)] 
-        
+    ds = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
     checkOne :: (Int, Int) -> Bool
-    checkOne (r, c) = (r <= h) && (r >= 1) && (c <= w) && (c >= 1) && (isSymbol (m ! (r, c)))
-    
+    checkOne (r, c) = inBounds (h, w) (r, c) && isSymbol (m ! (r, c))
+
     (_, (h, w)) = bounds m
+
+day03b :: Map -> String
+day03b = show . sum . findGears (1, 1)
 
 findGears :: (Int, Int) -> Map -> [Int]
-findGears (r, c) m 
-  | r == h && c == w + 1 = []  
-  | m ! (r, c) == '*'    = (if length adjNumbers == 2 then ....() else) ++ findGears next m 
-  | otherwise            = findGears next m
+findGears (r, c) m
+  | r == h + 1 && c == 1 = []
+  | m ! (r, c) == '*'    = if length adjNumbers == 2 then (x * y):rest else rest
+  | otherwise            = rest
   where
     (_, (h, w)) = bounds m
-    adj = getNumbers (r, c) m
-    next = if c == w + 1 then (r + 1, 1) else (r, c + 1)
     adjNumbers = numbersAround (r, c) m
+    (x : y : _) = adjNumbers
+    rest = findGears (if c == w then (r + 1, 1) else (r, c + 1)) m
 
 numbersAround :: (Int, Int) -> Map -> [Int]
+numbersAround (r, c) m = map read (catMaybes [left, right]) ++ top ++ bot
+  where left  = getLeft (r, c - 1) m
+        right = getRight (r, c + 1) m
+        topL  = getLeft (r - 1, c - 1) m
+        topR  = getRight (r - 1, c + 1) m
+        botL  = getLeft (r + 1, c - 1) m
+        botR  = getRight (r + 1, c + 1) m
+        top   = if inBoundsAndDigit (r - 1, c) m
+                  then combineLMandR topL (m ! (r - 1, c)) topR
+                  else landRToList topL topR
+        bot   = if inBoundsAndDigit (r + 1, c) m
+                  then combineLMandR botL (m ! (r + 1, c)) botR
+                  else landRToList botL botR
+        
+        combineLMandR :: Maybe String -> Char -> Maybe String -> [Int]
+        combineLMandR left chm right = [read (fromMaybe "" left ++ [chm] ++ fromMaybe "" right)]
 
--- find numbers around -- left, right, if middle is empty then check up left and up right separately otherwise check as one effectively. same for below
+        landRToList :: Maybe String -> Maybe String -> [Int] 
+        landRToList l r = map read (catMaybes [l, r])
 
-inp :: String
-inp = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+getLeft :: (Int, Int) -> Map -> Maybe String
+getLeft (r, c) m = fmap reverse (extractNumber (r, c) (0, -1) m)
+
+getRight :: (Int, Int) -> Map -> Maybe String
+getRight (r, c) = extractNumber (r, c) (0, 1)
+
+extractNumber :: (Int, Int) -> (Int, Int) -> Map -> Maybe String
+extractNumber (r, c) (dr, dc) m
+  | not (inBoundsAndDigit (r, c) m) = Nothing
+  | otherwise                       = fmap (d:) (extractNumber (r + dr, c + dc) (dr, dc) m <|> Just "")
+  where d = m ! (r, c)
 
 main :: IO ()
-main = adventOfCode 3 (day03a . arrayParse) (day03b . day03Parse)
-
+main = adventOfCode 3 (day03a . arrayParse) (day03b . arrayParse)
