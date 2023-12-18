6cc9d877522bbb9d61a5ef33fcef22fe16fe3050
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-12-18 13:39:12 +0000

    Solve Day 7 Part B

--- exe/Day07.hs
+++ exe/Day07.hs
@@ -4,7 +4,7 @@ module Main where
 --Base
 import Text.Gigaparsec (Parsec, some)
 import Text.Gigaparsec.Char (letterOrDigit, space)
-import Data.List (sortOn, sort)
+import Data.List (sortOn, sort, maximumBy, group)
 import Data.Ord (comparing)
 import qualified Data.Set as S 
 
@@ -16,27 +16,32 @@ type Bid = Int
 type Rank = Int
 newtype Hand = Hand [Card] deriving (Eq, Show)
 
-data Card = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Eq, Ord)
+data Card = Joker | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Eq, Ord)
 data HandType = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind deriving (Eq, Ord, Show)
 data Game = Game {hand :: Hand, bid :: Bid} deriving Show
 
-readCard :: Char -> Card
-readCard '2' = Two
-readCard '3' = Three
-readCard '4' = Four
-readCard '5' = Five
-readCard '6' = Six
-readCard '7' = Seven
-readCard '8' = Eight
-readCard '9' = Nine
-readCard 'T' = Ten
-readCard 'J' = Jack
-readCard 'Q' = Queen
-readCard 'K' = King
-readCard 'A' = Ace
-readCard _   = error "Malformed Card"
+readCardA :: Char -> Card
+readCardA '2' = Two
+readCardA '3' = Three
+readCardA '4' = Four
+readCardA '5' = Five
+readCardA '6' = Six
+readCardA '7' = Seven
+readCardA '8' = Eight
+readCardA '9' = Nine
+readCardA 'T' = Ten
+readCardA 'J' = Jack
+readCardA 'Q' = Queen
+readCardA 'K' = King
+readCardA 'A' = Ace
+readCardA _   = error "Malformed Card"
+
+readCardB :: Char -> Card
+readCardB 'J' = Joker
+readCardB x   = readCardA x
 
 instance Show Card where
+  show Joker = "J"
   show Two   = "2"
   show Three = "3"
   show Four  = "4"
@@ -54,38 +59,43 @@ instance Show Card where
 instance Ord Hand where
   compare ha@(Hand h) hb@(Hand i) = (comparing classifyHand ha hb) <> (compare h i)
 
+listReplace :: (Eq a) => a -> a -> [a] -> [a]
+listReplace _ _ [] = []
+listReplace s t (x:xs)
+  | s == x    = t:listReplace s t xs
+  | otherwise = x:listReplace s t xs  
+
 classifyHand :: Hand -> HandType
 classifyHand (Hand [])                                          = error "Empty hand!"
 classifyHand (Hand h@(c:cs))
   | all (== c) cs                                               = FiveOfAKind
-  | all (== s) [t, u, v] || all (== t) [u, v, w]                = FourOfAKind  
+  | S.member Joker (S.fromList h)                               = classifyHand (Hand (listReplace Joker mostCommonCard h))
+  | all (== s) [t, u, v] || all (== t) [u, v, w]                = FourOfAKind
   | unique == 2                                                 = FullHouse
   | all (== s) [t, u] || all (== t) [u, v] || all (== u) [v, w] = ThreeOfAKind
   | unique == 3                                                 = TwoPair
   | unique == 4                                                 = OnePair
   | otherwise                                                   = HighCard
-  where (s:t:u:v:w:[]) = sort h
+  where sorted@(s:t:u:v:w:[]) = sort h
         unique         = length (S.fromList h)
+        mostCommonCard = fst . maximumBy (comparing snd) $ [(c, length group) | group@(c:cs) <- group (filter (/= Joker) sorted)]
 
-parseCard :: Parsec Card
-parseCard = readCard <$> (letterOrDigit)
+parseCard :: (Char -> Card) -> Parsec Card
+parseCard readCard = readCard <$> (letterOrDigit)
 
-parseGame :: Parsec Game 
-parseGame = Game <$> ((Hand <$> some parseCard) <* (space)) <*> (parsePositiveInteger)
+parseGame :: (Char -> Card) -> Parsec Game 
+parseGame readCard = Game <$> ((Hand <$> some (parseCard readCard)) <* (space)) <*> (parsePositiveInteger)
 
-parseDay07 :: Input -> [Game] 
-parseDay07 = map (parseWellFormed parseGame) . lines 
+parseDay07 :: (Char -> Card) -> Input -> [Game] 
+parseDay07 readCard = map (parseWellFormed (parseGame readCard)) . lines 
 
 getWinning :: (Rank, Game) -> Int
 getWinning (rank, Game hand bid) = bid * rank
 
-day07a :: [Game] -> Int
-day07a games = sum . map getWinning . zip [1..] . sortOn hand $ games
-
-day07b :: [Game] -> Int
-day07b = undefined
+day07 :: [Game] -> Int
+day07 games = sum . map getWinning . zip [1..] . sortOn hand $ games
 
 inp = "32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483"
 
 main :: IO ()
-main = adventOfCode 7 parseDay07 day07a day07b
+main = adventOfCode 7 id (day07 . parseDay07 readCardA) (day07 . parseDay07 readCardB)
